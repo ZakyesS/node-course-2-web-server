43c2d0e17dba911384c3853635165a6f2d8f56aa
ysuarez20h <ysuarez@20h.es>
2018-04-04 23:12:36 +0100

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000; // process.env --> rutas donde estan todos los procesos, incluidos los del sistema y PORT el puerto que .
 //app es una instancia de express.
 let app = express();
 hbs.registerPartials(__dirname + '/views/partials'); //va a usar el directorio donde esten todos los handlebars partial.
@@ -75,8 +76,8 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request!.',
     });
 });
-app.listen(3000, () => {
-    console.log('Sever is up on port 3000');
+app.listen(port, () => {
+    console.log(`Sever is up on port ${port}`);
 }); //puerto usado para escuchar.
 
 
